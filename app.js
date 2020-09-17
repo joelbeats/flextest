d1582a824c8a241e5d39c7a8ed4c18127af512c2
Joel Hjelmstedt <joelbeats@gmail.com>
2020-09-17 10:07:50 +0200

    Fixed css added app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log("Welcome to my Portfolio!");
+
+var one = 3;
+var two = 2;
+var sum;
+var error = "You got it wrong";
+function calculate(){
+sum = one + two
+}
+
+if(one<two){
+    calculate();
+    console.log(sum)
+    
+} else {
+    console.log(error)
+}
+
